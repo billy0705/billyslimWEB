ce1d0229fc566378658fbcade676d5e074f8ebc2
billy0705 <billyg123@gmail.com>
2023-05-04 20:26:44 +0700

    rename phpinfo

--- Home.php
+++ Home.php
@@ -1,5 +1,5 @@
 <?php
-    echo "hello everyone!!!";
-    echo "<a href='./phpinfo.php'>phpinfo</a>";
-    echo phpinfo();
+    echo "hello everyone!!!<br>";
+    echo "<a href='./infophp.php'>phpinfo</a><br>";
+    echo "<a href='./test.php'>test</a><br>";
 ?>
