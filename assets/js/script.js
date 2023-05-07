25bd12aef52c57e3fe65918fc272375793f570d4
billy0705 <billyg123@gmail.com>
2023-05-07 21:19:05 +0700

    Add navbar, about page and resume

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,31 @@ const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 sidebarBtn.addEventListener("click", function () {
     elementToggleFunc(sidebar);
 });
+
+// page navigation variables
+const navigationLinks = document.querySelectorAll("[data-nav-link]");
+const pages = document.querySelectorAll("[data-page]");
+
+// add event to all nav link
+for (let i = 0; i < navigationLinks.length; i++) {
+    navigationLinks[i].addEventListener("click", function () {
+        // console.log(pages[i].classList);
+        // if (myElement.classList.contains('active')) {
+        // }
+        for (let i = 0; i < pages.length; i++) {
+            if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+                if (pages[i].classList.contains("active")) {
+                    pages[i].classList.remove("active");
+                    navigationLinks[i].classList.remove("active");
+                } else {
+                    pages[i].classList.add("active");
+                    navigationLinks[i].classList.add("active");
+                }
+                window.scrollTo(0, 0);
+            } else {
+                pages[i].classList.remove("active");
+                navigationLinks[i].classList.remove("active");
+            }
+        }
+    });
+}
